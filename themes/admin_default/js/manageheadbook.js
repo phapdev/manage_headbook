b21d89ec67b8a30d33ea5a2376db39dfce2a2bda
NguyetQue <ntnq1412@gmail.com>
2022-12-08 11:57:49 +0700

    Add, edit, delete subject

--- themes/admin_default/js/manageheadbook.js
+++ themes/admin_default/js/manageheadbook.js
@@ -13,3 +13,19 @@ function nv_del_class(ma_lop, checkss) {
 	}
 	return false;
 }
+
+function nv_del_subject(ma_mon_hoc, checkss) {
+	if (confirm(nv_is_del_confirm[0])) {
+		$.post(script_name + '?' + nv_lang_variable + '=' + nv_lang_data + '&' + nv_name_variable + '=' + nv_module_name + '&' + nv_fc_variable + '=delsubject&nocache=' + new Date().getTime(), 'ma_mon_hoc=' + ma_mon_hoc + '&checkss=' + checkss, function(res) {
+			var r_split = res.split("_");
+			if (r_split[0] == 'OK') {
+				window.location.href = script_name + '?' + nv_lang_variable + '=' + nv_lang_data + '&' + nv_name_variable + '=' + nv_module_name + '&' + nv_fc_variable + '=subject';
+			} else if (r_split[0] == 'ERR') {
+				alert(r_split[1]);
+			} else {
+				alert(nv_is_del_confirm[2]);
+			}
+		});
+	}
+	return false;
+}
