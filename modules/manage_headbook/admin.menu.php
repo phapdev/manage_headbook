b7cec3d430ffe55aaeaddd82982934d2e8c23ddd
phapdev <luongphap1810@gmail.com>
2022-12-08 08:33:28 +0700

    edit module name

---
+++ modules/manage_headbook/admin.menu.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Headbook Management System
+ * @version 4.x
+ * @author Group DNTU
+ * @license GNU/GPL version 3 
+ * @see https://github.com/phapdev/manage_headbook.git The Manage headbook GitHub project
+ */
+
+if (!defined('NV_ADMIN')) {
+    exit('Stop!!!');
+}
+
+$submenu[''] = $lang_module[''];
+
