2ff5f13554293bb6191897a952bced4553b720c0
phapdev <luongphap1810@gmail.com>
2022-12-08 08:59:22 +0700

    Rename modul

---
+++ modules/manageheadbook/admin/main.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Headbook Management System
+ * @version 4.x
+ * @author Group DNTU
+ * @license GNU/GPL version 3 
+ * @see https://github.com/phapdev/manage_headbook.git The Manage headbook GitHub project
+ */
+
+if (!defined('NV_IS_FILE_ADMIN')) {
+    exit('Stop!!!');
+}
+
+$page_title = $lang_module[''];
+
+
+include NV_ROOTDIR . '/includes/header.php';
+echo nv_admin_theme($contents);
+include NV_ROOTDIR . '/includes/footer.php';
---
+++ modules/manageheadbook/theme.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Headbook Management System
+ * @version 4.x
+ * @author Group DNTU
+ * @license GNU/GPL version 3 
+ * @see https://github.com/phapdev/manage_headbook.git The Manage headbook GitHub project
+ */
+
+if (!defined('NV_IS_MOD_HEADBOOK')) {
+    exit('Stop!!!');
+}
